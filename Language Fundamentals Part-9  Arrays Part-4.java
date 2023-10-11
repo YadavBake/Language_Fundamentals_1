c1bcd94f938824e5bcae0fcab7b86ef2832a247c
yadav Bakelal <bake2101992@gmail.com>
2023-10-11 17:31:43 +0530

    Update Language Fundamentals Part-9  Arrays Part-4.java

--- Language Fundamentals Part-9  Arrays Part-4.java
+++ Language Fundamentals Part-9  Arrays Part-4.java
@@ -1,6 +1,6 @@
 
-															Language Fundamentals Part-9 || Arrays Part-4
-														===================================================
+											Language Fundamentals Part-9 || Arrays Part-4
+										     ===================================================
 
 ---------------------------------
  9. Array varialbe assignments. 
@@ -16,10 +16,10 @@
 		
 		int []  b = x; //valid
 
-																								char[]				int []
-		int []  c = ch; // invalid CE: incompatible type found: char[] , required: int[].   		|					|			
-											                                                    	|					|
-													                                               [C				   [I
+																	     char[]				int []
+		int []  c = ch; // invalid CE: incompatible type found: char[] , required: int[].   		                                |			   	   |			
+											                                                    	|			   	   |
+													                                        [C				   [I
  -> Which of the following pramotions will be performed automatically ? 
 
 	char--->int  //valid 
@@ -48,14 +48,14 @@
 		
 		
 		1. a = b;											------------------------------
-											a------X------->| 10 | 20 | 30 | 40 | 50 |60 |
-		2. b = a; 								|			------------------------------
-												|								
-												|								|
-												|---------->-----------			|
-											b------X------->| 70 | 80 |			|
-												   |		-----------			|
-												   |----------------------------- 	
+												a------X------->| 10 | 20 | 30 | 40 | 50 |60 |
+		2. b = a; 									|		------------------------------
+												|				|				
+												|				|
+												|---------->    -----------	|
+												b------X------->| 70 | 80 |	|
+												   |		-----------	|
+												   |-----------------------------
 
 
 	Case 3: Whenever we are assinging one array to another array the dimensions must be matched for example in the one dimensional int[] array we should provide one dimensional 
@@ -78,18 +78,18 @@
 	
 		class Test {
 			
-				public static void main(String args[]){												
-																		                    		for(int i=0; i<=3; i++);{
-					for (int i = 0; i<=args.length;i++)                                     			
-					{                                                                       		 System.out.println(args[i]);
+				public static void main(String args[]){											
+														for(int i=0; i<=3; i++);{
+					for (int i = 0; i<=args.length;i++)                                    			
+					{                                                                      		 System.out.println(args[i]);
 						System.out.println(args[i]);                                        }
 					}                                                                       
-				}                                                                           args[0]
-				                                                                            args[1]
-		                                                                                    args[2]
-		                                                                                    args[3]
+				}                                                                                       args[0]
+				                                                                            		args[1]
+		                                                                                    			args[2]
+		                                                                                    			args[3]
 		
-		java Test A B C 						java Test  A B                   java Test 
+		java Test A B C 			java Test  A B                   java Test 
 		                                                                         
 		A                                       A                                ArrayIndexOutOfBoundsException
 		B                                       B
@@ -103,14 +103,14 @@
 		Ex 2. 
 		
 		
-			class Test {														  -------------
-																		args--X-> | A | B | C |
-				public static void main(String args[]) 						  |	  ------------- 		
-				{															  |
-					String [] argh = {"X", "Y", "Z"};						  |	
-																			  |->-------------
-					args = argh ;									 argh -----> | X | Y | Z |	
-																				 -------------	
+			class Test {										        -------------
+													     args--X-> | A | B | C |
+				public static void main(String args[]) 						  |     ------------- 		
+				{										  |
+					String [] argh = {"X", "Y", "Z"};					  |	
+														  |---->-------------
+					args = argh ;							    argh -----> | X | Y | Z |	
+															-------------	
 					for (String s: args)
 					{
 						System.out.println(s);
@@ -118,38 +118,38 @@
 				}
 		
 		
-	java Test A B C 						java Test  A B                        java Test 
+	java Test A B C 				java Test  A B                        java Test 
 		                                        X                                 
-		X                                       Y                                  X
-		Y                                       Z								   Y	
-		Z                                       								   Z	
+		X                                       Y                                       X
+		Y                                       Z					Y	
+		Z                                       					Z	
 		
-																									----------------- ---------	
+														----------------- ---------	
 			                                                                                        | X | Y | Z | A | | X | Y |	
-																						            ----------------- ---------	
-	Ex 3.																								  |				|
-																										  |	  |----------
-																										-----------------
-		int [][] a = new int [4][3];												a-------X----------->| X | Y | Z | A |		
-		                                                                                |                -----------------	
-		a[0] = new int[4];																|				  |   |   |    |-------------------
-		a[1] = new int[2];																|		  --------X	  X   |						  |				
-																						|		  |			  |	  |--------|			  |		
-																						|	------------- -------------	------------- -------------							
-																						|	| X | Y | Z | | X | Y | Z | | X | Y | Z | | X | Y | Z |                 	
-		a = new int [3][2];                                         					|	------------- ------------- ------------- -------------                 	
-																					    |
-																			------------
-		1. Total how many object got created  ?								|		
-																			|
-			11.																|				-------------
+														----------------- ---------	
+	Ex 3.													  |		    |
+														  |  |---------------
+													     -----------------
+		int [][] a = new int [4][3];						a-------X----------->| X | Y | Z | A |		
+		                                                                                |            -----------------	
+		a[0] = new int[4];								|	        |   |   |    |-----------------------------
+		a[1] = new int[2];								|      -------- X   X   |    |				  |
+												|               |    |  |--------|			  |		
+												|	------------- ------------- ------------- -------------							
+												|	| X | Y | Z | | X | Y | Z | | X | Y | Z | | X | Y | Z |                 	
+		a = new int [3][2];                                         			|	------------- ------------- ------------- -------------                 	
+										    	        |
+												|
+		1. Total how many object got created  ?						|		
+												|
+			11.						|                       |	-------------
 			                                                                |------------>  | X | Y | Z |
-		2. Total how many object eligible for GC ?                                          -------------
-																						---- |   |	   |-----	
-			7. 																			|		 |			| 
-																					--------- ---------  ---------
-																					| X | Y | | x | Y |  | X | Y |
-																					--------- ---------  ---------
+		2. Total how many object eligible for GC ?	                                        -------------
+												     ---- |   |	   |----	
+			7. 									     |        |        |
+												--------- ---------  ---------
+												| X | Y | | x | Y |  | X | Y |
+												--------- ---------  ---------
 	
 	
 	
